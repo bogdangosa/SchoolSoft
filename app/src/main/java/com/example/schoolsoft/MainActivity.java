920c65013267aca9b9e8cd2e1ded32962f25cda4
bogdangosa <57357871+bogdangosa@users.noreply.github.com>
2021-02-15 16:49:27 +0200

    initial commit

---
+++ app/src/main/java/com/example/schoolsoft/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.schoolsoft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
